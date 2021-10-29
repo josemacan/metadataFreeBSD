7319a3f110288d970fccb9eb2bd5689b7e18b0cd
josemacan <jmcancinos97@gmail.com>
2021-10-29 07:57:20 -0300

    sprintf malloc func1

--- sys/kern/metadata_elf_reader.c
+++ sys/kern/metadata_elf_reader.c
@@ -260,7 +260,7 @@ int copyMetadataToProc(char *metadata, struct thread *td){
 char* getPayloadPerFunction(struct thread *td, int num_function, int* return_flag){ 
 
 	//int func1_len = FUNCTION_NDIGITS+1+1+1;
-	int func1_len = 50;
+	//int func1_len = 50;
 
 	if(num_function == UND){
 		log(LOG_INFO, "\t\t 3) // getPayloadPerFunction() // LectorELF // -- ERROR - function number is 0 - INVALID\n");
