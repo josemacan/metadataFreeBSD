c85e57fe19e87fffd72f3086ebead42bfd94314c
josemacan <jmcancinos97@gmail.com>
2021-10-28 22:30:55 -0300

    snprintf func1 getPayloadPerFunction

--- sys/kern/metadata_elf_reader.c
+++ sys/kern/metadata_elf_reader.c
@@ -275,8 +275,8 @@ char* getPayloadPerFunction(struct thread *td, int num_function, int* return_fla
 	//////////////////
 
 	char func1[FUNCTION_NDIGITS+1+1+1];
-	func1[FUNCTION_NDIGITS+1+1]='\0';
-	sprintf(func1, "[%d:",num_function);
+	//func1[FUNCTION_NDIGITS+1+1]='\0';
+	snprintf(func1, FUNCTION_NDIGITS+1+1+1, "[%d:",num_function);
 
 	
 	////////////////
